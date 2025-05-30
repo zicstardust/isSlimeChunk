f1521a1cd598055fea4ab066c2e181a52aeed1af
Zic <git@zicstardust.com>
2025-05-30 16:52:31 -0400

    Fix arguments error exception

--- src/main/java/com/zicstardust/isSlimeChunk/SlimeChunkCommand.java
+++ src/main/java/com/zicstardust/isSlimeChunk/SlimeChunkCommand.java
@@ -11,8 +11,28 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class SlimeChunkCommand implements CommandExecutor {
+
+    public enum Position  {
+        X,
+        Z;
+    }
+
+    private double positionValidator(@NotNull Position position,  @NotNull String argument,  @NotNull Player player) {
+
+        if (argument.equals("~")){
+            if (position.equals(Position.X)){
+                return player.getLocation().getX();
+            } else {
+                return player.getLocation().getZ();
+            }
+        }
+        return Double.parseDouble(argument);
+    }
+
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
@@ -36,11 +56,17 @@ public class SlimeChunkCommand implements CommandExecutor {
             if (args.length == 0) {
                 check_chunk = player.getChunk().isSlimeChunk();
             } else if ( args.length == 2) {
+
+                if (((!Pattern.matches("[0-9]+", args[0])) && (!args[0].equals("~"))) || ((!Pattern.matches("[0-9]+", args[1])) && (!args[1].equals("~")))) {
+                    player.sendMessage(textWrongArguments);
+                    return true;
+                }
+
                 check_chunk = new Location(
-                            player.getLocation().getWorld(),
-                            Double.parseDouble(args[0]),
-                            player.getLocation().getY(),
-                            Double.parseDouble(args[1])
+                        player.getLocation().getWorld(),
+                        positionValidator(Position.X, args[0], player),
+                        player.getLocation().getY(),
+                        positionValidator(Position.Z, args[1], player)
                         ).getChunk().isSlimeChunk();
             } else {
                 player.sendMessage(textWrongArguments);
